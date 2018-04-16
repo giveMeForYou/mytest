f431fa925e25da1d9256030711362a517961c33f
giveMeForYou <38397494+giveMeForYou@users.noreply.github.com>
2018-04-16 13:59:53 +0800

    提交

--- MyInternetTest/Form1.cs
+++ MyInternetTest/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net;
 namespace MyInternetTest
 {
     public partial class Form1 : Form
@@ -15,6 +15,18 @@ namespace MyInternetTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = string.Empty;
+            //获取主机地址
+            IPAddress[] ips = Dns.GetHostAddresses(textBox1.Text);
+            //循环遍历IP地址
+            foreach (IPAddress ip in ips) {
+                //在lable2中显示IP的信息
+                label2.Text = "网际协议地址：" + ip.Address + "\nIP地址的地址家族：" + ip.AddressFamily.ToString()+"\n是否ipv6连接本地地址："+ip.IsIPv6LinkLocal;
+            }
         }
     }
 }
